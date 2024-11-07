cda0cac4a5a2ec8bf25f201130e7085a38432649
Timothy Li <timmyli3481@gmail.com>
2024-11-06 23:05:06 -0800

    Refactor RobotTeleopPOV_Linear.java for improved readability.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
@@ -6,10 +6,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="Robot: Teleop POV", group="Robot")
 public class RobotTeleopPOV_Linear extends LinearOpMode {
     public RobotHardware robot = new RobotHardware(this);
+
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode(){
         robot.init(hardwareMap);
-        telemetry.addData(">", "Robot Ready.  Press START.");    //
+        telemetry.addData(">", "Robot Ready.  Press START.");
         telemetry.update();
         waitForStart();
         telemetry.addData("Status", "Initialized");
@@ -18,19 +19,19 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         while (opModeIsActive()) {
             if (gamepad1.right_bumper) {
                 robot.setArmPower(0.5);
-            }
-            else if (gamepad1.right_trigger > 0.5) {
+            } else if (gamepad1.right_trigger > 0.5) {
                 robot.setArmPower(-0.5);
-            }
-            else{
+            } else {
                 robot.setArmPower(0);
             }
-            robot.drive(gamepad1.left_stick_x, gamepad1.left_stick_y*0.5, gamepad1.right_stick_x);
+
+            double drive = -gamepad1.left_stick_y * 0.5;
+            double strafe = gamepad1.left_stick_x * 0.5;
+            double rotate = gamepad1.right_stick_x * 0.5;
+            robot.drive(strafe, drive, rotate);
+
             telemetry.addData("Status", "Running");
             telemetry.update();
-
         }
-
-
     }
 }
