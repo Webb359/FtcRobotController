83ea8cb0e6adf644b1c9fa208986a44635126f5b
Timothy Li <timmyli3481@gmail.com>
2024-11-06 22:54:13 -0800

    New codebase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
@@ -1,182 +1,35 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
-import android.annotation.SuppressLint;
-
-import androidx.appcompat.view.menu.ActionMenuItemView;
-
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-/*
- * This OpMode executes a POV Game style Teleop for a direct drive robot
- * The code is structured as a LinearOpMode
- *
- * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
- * It raises and lowers the arm using the Gamepad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 
 @TeleOp(name="Robot: Teleop POV", group="Robot")
-//@Disabled
 public class RobotTeleopPOV_Linear extends LinearOpMode {
-
-
-    /* Declare OpMode members. */
-    public DcMotor leftFront = null;
-    public DcMotor leftBack = null;
-    public DcMotor rightBack = null;
-    public DcMotor rightFront = null;
-    public DcMotor leftArm = null;
-    public DcMotor rightArm = null;
-    public Servo claw = null;
-
-    double clawOffset = 0;
-    //
-    public static final double MID_SERVO = 0.5;
-    public static final double CLAW_SPEED = 10;                 // sets rate to move servo
-    //I'm a skibidi ohio rizzler
-
-    @SuppressLint("RestrictedApi")
+    public RobotHardware robot = new RobotHardware(this);
     @Override
-    public void runOpMode() {
-
-
-        // Define and Initialize Motors+
-        leftFront = hardwareMap.get(DcMotor.class, "left_front");
-        rightFront = hardwareMap.get(DcMotor.class, "right_front");
-        leftBack = hardwareMap.get(DcMotor.class, "left_back");
-        rightBack = hardwareMap.get(DcMotor.class, "right_back");
-        leftArm = hardwareMap.get(DcMotor.class, "left_arm");
-        rightArm = hardwareMap.get(DcMotor.class, "right_arm");
-        claw = hardwareMap.get(Servo.class, "claw");
-
-
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftFront.setDirection(DcMotor.Direction.FORWARD);
-        rightFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack.setDirection(DcMotor.Direction.FORWARD);
-        rightBack.setDirection(DcMotor.Direction.REVERSE);
-
-
-
-        leftArm.setDirection(DcMotor.Direction.FORWARD);
-        rightArm.setDirection(DcMotor.Direction.FORWARD);
-
-
-        // Retrieve the IMU from the hardware map
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-        // Adjust the orientation parameters to match your robot
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-        imu.initialize(parameters);
-
-
-        // Send telemetry message to signify robot waiting;
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        waitForStart();
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
         telemetry.addData(">", "Robot Ready.  Press START.");    //
         telemetry.update();
-
-        // Wait for the game to start (driver presses START)
-        waitForStart();
-        double y = 0; // Remember, Y stick value is reversed
-        double x = 0;
-        double rx = 0;
-        boolean up;
-        double down = 0;
-
-        // run until the end of the match (driver presses STOP)
-        if (isStopRequested()) return;
-
         while (opModeIsActive()) {
-            y = -gamepad1.left_stick_y * 0.5;
-            x = gamepad1.left_stick_x; // Counteract imperfect strafing
-            rx = gamepad1.right_stick_x;
-            up = gamepad1.right_bumper;
-            down = gamepad1.right_trigger;
+            if (gamepad1.right_bumper) {
+                robot.setArmPower(0.5);
+            }
+            else if (gamepad1.right_trigger > 0.5) {
+                robot.setArmPower(-0.5);
+            }
+            else{
+                robot.setArmPower(0);
+            }
+            robot.drive(gamepad1.left_stick_x, gamepad1.left_stick_y*0.5, gamepad1.right_stick_x);
+            telemetry.addData("Status", "Running");
 
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-            if (up)
-            {
-                leftArm.setPower(.5);
-                rightArm.setPower(.5);
-            }
-            else {
-                leftArm.setPower(0);
-                rightArm.setPower(0);
-            }
-            if (gamepad1.right_trigger > 0)
-            {
-                leftArm.setPower(-down);
-                rightArm.setPower(-down);
-            }
-            else
-            {
-                leftArm.setPower(0);
-                rightArm.setPower(0);
-            }
-
-            leftFront.setPower(frontLeftPower);
-            leftBack.setPower(-backLeftPower);
-            rightFront.setPower(frontRightPower);
-            rightBack.setPower(backRightPower);
-            //servo range is 0-180 degrees(0 = 0 degrees, 1 = 180 degrees)
-//            if (gamepad1.right_bumper) {
-//                claw.setPosition(1);
-//            }
-//            if (gamepad1.left_bumper) {
-//                claw.setPosition(0);
-            }
 
         }
+
+
     }
+}
