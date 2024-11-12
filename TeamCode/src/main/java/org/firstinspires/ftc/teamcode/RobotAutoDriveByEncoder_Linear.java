7b0abac5049c2f28818908f8c8da13524864c69c
Timothy Li <timmyli3481@gmail.com>
2024-11-12 15:21:01 -0800

    ``` feat: update auto drive routine and add arm control (issue #123)
    - Adjusted initial drive distance from 20 to 27 inches.
    - Added new movements for arm control to pick up and score items.
    - Implemented timed arm power adjustments for better control.
    ```

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByEncoder_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByEncoder_Linear.java
@@ -2,10 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Robot: Auto Drive By Encoder Linear", group="Robot")
 public class RobotAutoDriveByEncoder_Linear extends LinearOpMode {
     public RobotHardware robot = new RobotHardware(this);
+    private ElapsedTime runtime = new ElapsedTime();
 
     public void runOpMode() {
         robot.init(hardwareMap);
@@ -16,9 +18,25 @@ public class RobotAutoDriveByEncoder_Linear extends LinearOpMode {
         waitForStart();
         telemetry.addData("Status", "Running");
         telemetry.update();
-        robot.drive_encoders(0, 20, 48, 5);
+        robot.drive_encoders(0, 27, 48, 5);
+        // move arm to pick up thing
+        robot.drive_encoders(0, 105, 48, 5);
+        robot.rotate_encoders(90, 48, 5);
+        // move arm to score
+        robot.drive_encoders(0, 15.5, 48, 5);
+        robot.rotate_encoders(-90, 48, 5);
+        // move arm to place thing
+        runtime.reset();
 
-
+        while (opModeIsActive() && runtime.seconds() < 1) {
+            robot.setArmPower(50);
+        }
+        while (opModeIsActive() && runtime.seconds() < 2) {
+            robot.setArmPower(-30);
+        }
+        robot.setArmPower(0);
+        robot.drive_encoders(0, -10, 48, 5);
+        robot.stopDrive();
 /*
     drive: (inches)
     forward: 27
